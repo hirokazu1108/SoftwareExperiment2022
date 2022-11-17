b2ede8a5b9e9c31bd98eaea8efaae59b675978d3
OgitaMasaki <c612101171@tokushima-u.ac.jp>
2022-11-17 15:35:47 +0900

    dキーで弾の発射を実装しました

---
+++ masao/client.h
@@ -0,0 +1,34 @@
+#ifndef _CLIENT_FUNC_H_
+#define _CLIENT_FUNC_H_
+
+#include"../constants.h"
+#include <joyconlib.h>
+
+#define WINDOW_PosX 0
+#define WINDOW_PosY 0
+#define WINDOW_WIDTH 2000
+#define WINDOW_HEIGHT 2000
+
+/* client_net.cpp */
+extern int SetUpClient(char *hostName,u_short port,int *clientID,int *num,char clientNames[][MAX_NAME_SIZE]);
+extern void CloseSoc(void);
+extern int RecvIntData(int *intData);
+extern void SendData(void *data,int dataSize);
+extern int SendRecvManager(void);
+extern int RecvData(void *data,int dataSize);
+
+
+/* client_win.cpp */
+extern int InitWindows(int clientID,int num,char name[][MAX_NAME_SIZE]);
+extern void DestroyWindow(void);
+extern void WindowEvent(int num);
+
+/* client_command.cpp */
+extern int ExecuteCommand(char command);
+extern void SendEndCommand(void);
+extern void SendPlayerDataCommand(void);
+
+extern int		gClientNum;
+extern int		clientID;
+extern Player *player;
+#endif
