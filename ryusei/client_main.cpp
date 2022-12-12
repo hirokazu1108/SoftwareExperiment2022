68341fdd4da91f70777a67b1e9c333dbf9aae12d
RyuseiSakata <c612101505@tokushima-u.ac.jp>
2022-12-12 15:29:59 +0900

    hennkousimasita

--- ryusei/client_main.cpp
+++ ryusei/client_main.cpp
@@ -97,7 +97,7 @@ int joyconev();
 #define TEX_HEIGHT 32
 #define TEX_WIDTH 32
 static GLubyte image[TEX_HEIGHT][TEX_WIDTH][4];
-const char *modelname = "sentouki.obj" 
+
 
 /***********************************************************
 |  �ؿ���main()
@@ -113,7 +113,8 @@ int main(int argc, char **argv)
     glutInit(&argc, argv); /* OpenGL �ν���� */
     myInit(argv[0]);       /* ������ɥ�ɽ������������ν���� */
     
-    model_file = *modelname;
+   char* modelename = argv[1];
+    model_file = modelename;
     if (!model_file) {
     fprintf(stderr, "usage: smooth model_file.obj\n");
     exit(1);
