be8c3d8a898e9724c9135a8a3adcb2a7f10cb3d6
HiRO <87222170+hirokazu1108@users.noreply.github.com>
2023-01-05 18:48:32 +0900

    機体の初期化関数などを追加

---
+++ server/server_func.cpp
@@ -0,0 +1,47 @@
+#include "server.h"
+
+int maxButtonNum[SCENE_NUM]={};    //シーンごとのボタンの最大数,indexはScene列挙体に対応
+
+void PlayerInit(void){
+    /* キャラ情報の初期化 */
+	player = (Player*)malloc(sizeof(Player)*gClientNum);
+	for(int i = 0; i< gClientNum;i++)
+    {
+        player[i].enabled = true;
+        player[i].spead = 0.0;
+        player[i].dir.x = 0;
+        player[i].dir.y = 0;
+        player[i].dir.z = 0;
+        player[i].pos.x = 0;
+        player[i].pos.y = 0;
+        player[i].pos.z = 0;
+        player[i].upVec.x = 0;
+        player[i].upVec.y = 0;
+        player[i].upVec.z = 0;
+        player[i].turn1 = 0;
+        player[i].turn2 = 0;
+        player[i].turn3 = 0;
+        player[i].type = 0;
+        player[i].mp = 0;
+        player[i].hp = 3;
+        player[i].skill = SKILL_ATTACK;
+        player[i].special = SPECIAL_BIGBULLET;
+        for(int j=0; j<PARAMATER_NUM; j++)
+            player[i].parm[j] = 0;
+        player[i].reloadTime= 0;
+        player[i].collider.radius = 1.0;
+        player[i].collider.pos = player[i].pos;
+    }
+}
+
+/* 選択しているボタンの番号をずらす.引数に+1で次,-1で前のボタンへ */
+void shiftButtonSelect(int shift, int max){
+
+    gUi.selectButton += (shift>=0) ? shift%max : -(-shift)%max;
+    if(gUi.selectButton < 0){
+        gUi.selectButton += max;
+    }
+    else if(gUi.selectButton >= max){
+        gUi.selectButton -= max;
+    }
+}
