557fd8cb4201b1b85a1ad007c2b2615e7c74ddfe
HiRO <87222170+hirokazu1108@users.noreply.github.com>
2023-01-14 00:03:02 +0900

    tuika

--- client/client_win.cpp
+++ client/client_win.cpp
@@ -42,50 +42,6 @@ void uiSetting(void){
 /* 参考　http://gameprogrammingunit.web.fc2.com/gl/bitmap_character.htm */
 void DrawString(std::string str,int x0, int y0, void *font)
 {
-#include "client.h"
-
-/*UIの図形をこちらに向ける*/
-void uiSetting(void){
-    /*
-        glPushMatrix();
-    */
-    glTranslatef(player[clientID].pos.x, player[clientID].pos.y,player[clientID].pos.z);
-    glRotatef(player[clientID].turn1*57.5, 0, 1, 0);
-    glRotatef(player[clientID].turn2*57.5*-1, 1, 0, 0);
-
-    GLdouble m[16],l;
-    glGetDoublev(GL_MODELVIEW_MATRIX, m);
-    m[8] = -m[12];
-    m[9] = -m[13];
-    m[10] = -m[14];
-    l = sqrt(m[8] * m[8] + m[9] * m[9] + m[10] * m[10]);
-    m[8] /= l;
-    m[9] /= l;
-    m[10] /= l;
-    
-    m[0] = -m[14];
-    m[1] = 0.0;
-    m[2] = m[12];
-    l = sqrt(m[0] * m[0] + m[1] * m[1] + m[2] * m[2]);
-    m[0] /= l;
-    m[1] /= l;
-    m[2] /= l;
-
-    m[4] = m[9] * m[2] - m[10] * m[1];
-    m[5] = m[10] * m[0] - m[8] * m[2];
-    m[6] = m[8] * m[1] - m[9] * m[0];
-    glLoadMatrixd(m);
-    /* 
-        この関数の後に 
-        glRectf( 1, 1, 2,2);
-        glPopMatrix();
-    */
-}
-
-
-/* 参考　http://gameprogrammingunit.web.fc2.com/gl/bitmap_character.htm */
-void DrawString(std::string str,int x0, int y0, void *font)
-{
 
     // 画面上にテキスト描画
     glRasterPos2f(x0, y0);
@@ -105,14 +61,4 @@ void drawScoreBall(void){
         glutSolidSphere(ary_scoreBall[i].collider.radius, 16, 16);
         glPopMatrix();
     }
-}
-
-    // 画面上にテキスト描画
-    glRasterPos2f(x0, y0);
-    int size = (int)str.size();
-    for(int i = 0; i < size; ++i){
-        char ic = str[i];
-        glutBitmapCharacter(font, ic);
-    }
-
 }
