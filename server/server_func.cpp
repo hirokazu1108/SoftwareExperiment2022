3bf71d27967a4213d881dda5692107e8bd86bb1c
HiRO <87222170+hirokazu1108@users.noreply.github.com>
2023-01-17 20:52:27 +0900

    tuika

--- server/server_func.cpp
+++ server/server_func.cpp
@@ -34,6 +34,7 @@ void PlayerInit(void){
         player[i].kill_boss = 0;
         player[i].isBarrier = 0.0f;
         player[i].isDisable = 0.0f;
+	player[i].isSpecial = 0.0f;
         player[i].ability = UP_ATTACK;
         player[i].skill = SKILL_ATTACK;
         player[i].special = SPECIAL_BIGBULLET;
