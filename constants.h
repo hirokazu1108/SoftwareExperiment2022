ed1793cc8d0daa0088ae7499baf5a037de845e92
RyuseiSakata <c612101505@tokushima-u.ac.jp>
2023-01-20 19:10:55 +0900

    ghennkou

--- constants.h
+++ constants.h
@@ -62,7 +62,7 @@
 #define SPECIAL_NUM 8            //num of specials
 #define FILENAME_GAMEDATA "../data/gamedata.bin"
 #define FILENAME_RANKINGDATA "../data/ranking.txt"
-#define GAMETIME 100
+#define GAMETIME 300
 
 typedef enum {
     State_Wait,
